7c448645ac74499946d89ba93a78f9f88c43f13b
Alvaro Tedeschi <alvaro_porc@hotmail.com>
2021-08-17 23:39:08 -0300

    Started frontend in /mobile

---
+++ mobile/android/app/src/main/kotlin/com/example/mebook/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.mebook
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ mobile/lib/main.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'mebook',
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('meebok'),
+      ),
+      body: Column(
+        children: [Text('MEBOOK')],
+      ),
+    );
+  }
+}
