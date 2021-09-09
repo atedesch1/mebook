1244080c4a40bd85c947f166135d3a8dd06e7219
Alvaro Tedeschi <alvaro_porc@hotmail.com>
2021-09-09 00:38:19 -0300

    Changed accentColor and primarySwatch for temporary fix of end of scroll animation

--- mobile/lib/res/theme.dart
+++ mobile/lib/res/theme.dart
@@ -2,52 +2,54 @@ import 'package:flutter/material.dart';
 
 class MebookTheme {
   ThemeData get themeData => ThemeData(
-      primaryColor: Colors.white,
-      primarySwatch: Colors.amber,
-      backgroundColor: Colors.amber,
-      fontFamily: 'TruenoRound',
-      elevatedButtonTheme: ElevatedButtonThemeData(
-        style: ButtonStyle(
-          padding: MaterialStateProperty.all<EdgeInsets>(
-            EdgeInsets.symmetric(vertical: 16),
-          ),
-          backgroundColor: MaterialStateProperty.all<Color>(Colors.black),
-          foregroundColor: MaterialStateProperty.all<Color>(Colors.white),
-          shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-            RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(100),
+        primaryColor: Colors.white,
+        accentColor: Colors.grey,
+        // primarySwatch: Colors.amber,
+        backgroundColor: Colors.amber,
+        fontFamily: 'TruenoRound',
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ButtonStyle(
+            padding: MaterialStateProperty.all<EdgeInsets>(
+              EdgeInsets.symmetric(vertical: 16),
+            ),
+            backgroundColor: MaterialStateProperty.all<Color>(Colors.black),
+            foregroundColor: MaterialStateProperty.all<Color>(Colors.white),
+            shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+              RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(100),
+              ),
+            ),
+            textStyle: MaterialStateProperty.all<TextStyle>(
+              TextStyle(fontWeight: FontWeight.bold),
             ),
           ),
-          textStyle: MaterialStateProperty.all<TextStyle>(
-            TextStyle(fontWeight: FontWeight.bold),
+        ),
+        textButtonTheme: TextButtonThemeData(
+          style: ButtonStyle(
+            foregroundColor: MaterialStateProperty.all<Color>(Colors.black),
           ),
         ),
-      ),
-      textButtonTheme: TextButtonThemeData(
-        style: ButtonStyle(
-          foregroundColor: MaterialStateProperty.all<Color>(Colors.black),
+        inputDecorationTheme: InputDecorationTheme(
+          fillColor: Colors.grey.shade200,
+          filled: true,
+          contentPadding: EdgeInsets.symmetric(
+            horizontal: 30,
+            vertical: 18,
+          ),
+          border: OutlineInputBorder(
+            borderSide: BorderSide.none,
+            borderRadius: BorderRadius.circular(100),
+          ),
+          labelStyle: TextStyle(
+            fontWeight: FontWeight.bold,
+          ),
         ),
-      ),
-      inputDecorationTheme: InputDecorationTheme(
-        fillColor: Colors.grey.shade200,
-        filled: true,
-        contentPadding: EdgeInsets.symmetric(
-          horizontal: 30,
-          vertical: 18,
+        snackBarTheme: SnackBarThemeData(
+          behavior: SnackBarBehavior.floating,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.all(Radius.circular(10)),
+          ),
+          backgroundColor: Colors.red,
         ),
-        border: OutlineInputBorder(
-          borderSide: BorderSide.none,
-          borderRadius: BorderRadius.circular(100),
-        ),
-        labelStyle: TextStyle(
-          fontWeight: FontWeight.bold,
-        ),
-      ),
-      snackBarTheme: SnackBarThemeData(
-        behavior: SnackBarBehavior.floating,
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.all(Radius.circular(10)),
-        ),
-        backgroundColor: Colors.red,
-      ));
+      );
 }
