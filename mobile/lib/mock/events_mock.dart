57b88a839ed351441bb367d1d8a63a91e3289cbc
vasconcelosdavi <davi.muniz41@gmail.com>
2021-11-11 22:33:04 -0300

    Add events mock.

---
+++ mobile/lib/mock/events_mock.dart
@@ -0,0 +1,25 @@
+class EventsMock {
+  final List<Map<String, Object>> events = [
+    {
+      'id': 0,
+      'title': 'Event 1',
+      'startTime': '2021-11-11 13:30:00',
+      'endTime': '2021-11-11 14:00:00',
+      'notify': false
+    },
+    {
+      'id': 1,
+      'title': 'Event 2',
+      'startTime': '2021-11-11 15:30:00',
+      'endTime': '2021-11-11 16:00:00',
+      'notify': false
+    },
+    {
+      'id': 2,
+      'title': 'Event 3',
+      'startTime': '2021-11-11 17:30:00',
+      'endTime': '2021-11-11 18:20:00',
+      'notify': false
+    }
+  ];
+}
