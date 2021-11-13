61814fffd7dd1d0a118dc539238ef92003c7b71f
Alvaro Tedeschi <alvaro_porc@hotmail.com>
2021-11-13 18:17:51 -0300

    add transaction service

---
+++ mobile/lib/services/finances_service.dart
@@ -0,0 +1,79 @@
+import 'package:cloud_firestore/cloud_firestore.dart' hide Transaction;
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:mebook/models/transaction_model.dart';
+import 'package:mebook/services/auth_service.dart';
+import 'package:provider/src/provider.dart';
+
+class FinancesService {
+  final FirebaseFirestore _db = FirebaseFirestore.instance;
+  User _currentUser;
+
+  FinancesService(BuildContext context) {
+    this._currentUser = context.read<AuthService>().currentUser;
+  }
+
+  Future<void> createOrUpdateTransaction({
+    String docId,
+    @required title,
+    @required date,
+    @required amount,
+  }) {
+    final ref =
+        _db.collection('transaction').doc(_currentUser.uid).collection('items');
+
+    Map<String, dynamic> data = {
+      'title': title,
+      'date': date,
+      'amount': amount,
+    };
+
+    if (docId == null) {
+      return ref
+          .doc()
+          .set(data)
+          .whenComplete(() => print('Transaction created'))
+          .catchError((e) => print(e));
+    }
+
+    return ref
+        .doc(docId)
+        .update(data)
+        .whenComplete(
+          () => print('Transaction updated'),
+        )
+        .catchError(
+          (e) => print(e),
+        );
+  }
+
+  Stream<List<Transaction>> getTransactions() {
+    final ref = _db
+        .collection('transactions')
+        .doc(_currentUser.uid)
+        .collection('items');
+
+    return ref.snapshots().map((list) => list.docs.map(
+          (doc) {
+            return Transaction.fromFirestore(doc);
+          },
+        ).toList());
+  }
+
+  Future<void> deleteTransaction(String docId) async {
+    final ref = _db
+        .collection('transactions')
+        .doc(_currentUser.uid)
+        .collection('items')
+        .doc(docId);
+
+    return ref
+        .delete()
+        .whenComplete(
+          () => print('Transaction deleted'),
+        )
+        .catchError(
+          (e) => print(e),
+        );
+  }
+}
