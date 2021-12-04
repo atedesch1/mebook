07a4a6f38af88b5de1b71b7a2c92dc6f13136dde
Alvaro Tedeschi <alvaro_porc@hotmail.com>
2021-12-04 17:29:49 -0300

    add get events for month

--- mobile/lib/services/calendar_service.dart
+++ mobile/lib/services/calendar_service.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:googleapis/calendar/v3.dart';
+import 'package:mebook/models/event_model.dart';
 import 'package:mebook/services/auth_service.dart';
 import 'package:provider/src/provider.dart';
 
@@ -20,5 +21,14 @@ class CalendarService {
     return _api.events
         .list('primary', timeMin: firstDayOfMonth, timeMax: lastDayOfMonth)
         .asStream();
+  }
+
+  Future<Events> getMonthEvents(DateTime chosenDate) {
+    var firstDayOfMonth = new DateTime(chosenDate.year, chosenDate.month, 1);
+    var lastDayOfMonth = DateTime(chosenDate.year, chosenDate.month + 1, 1)
+        .subtract(Duration(seconds: 1));
+
+    return _api.events
+        .list('primary', timeMin: firstDayOfMonth, timeMax: lastDayOfMonth);
   }
 }
