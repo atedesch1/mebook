e526d29cd13623fbd2244e5c9ab250b99975cd69
Alvaro Tedeschi <alvaro_porc@hotmail.com>
2021-12-05 01:22:39 -0300

    add mock profile

--- mobile/lib/screens/profile_screen.dart
+++ mobile/lib/screens/profile_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:mebook/widgets/misc/overlay_app_bar.dart';
+import 'package:mebook/widgets/profile/simple_count_statistic.dart';
 import 'package:provider/provider.dart';
 
 import 'package:mebook/services/auth_service.dart';
@@ -7,6 +9,22 @@ import 'package:mebook/services/auth_service.dart';
 class ProfileScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final profilePictureURL =
+        context.read<AuthService>().currentUser.photoURL ?? null;
+
+    print(profilePictureURL);
+
+    final circleAvatar = profilePictureURL != null
+        ? CircleAvatar(
+            radius: MediaQuery.of(context).size.width * 0.14,
+            backgroundImage: NetworkImage(profilePictureURL),
+          )
+        : Icon(
+            Icons.account_circle,
+            color: Colors.black54,
+            size: MediaQuery.of(context).size.width * 0.14 * 2,
+          );
+
     return Scaffold(
       body: CustomScrollView(
         physics: const BouncingScrollPhysics(
@@ -16,23 +34,56 @@ class ProfileScreen extends StatelessWidget {
             title: 'Profile',
             actions: [
               IconButton(
-                key: ValueKey("signOutButton"),
                 onPressed: context.read<AuthService>().signOut,
                 icon: Icon(Icons.logout),
               ),
             ],
           ),
-          SliverPadding(
-            padding: EdgeInsets.symmetric(horizontal: 5),
-            sliver: SliverList(
-              delegate: SliverChildListDelegate(
-                [
-                  Padding(
-                    padding: const EdgeInsets.only(top: 5),
-                    child: Text('Profile'),
+          SliverFillRemaining(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(10.0),
+                  child: Row(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      circleAvatar,
+                      Expanded(
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          children: [
+                            SimpleCountStatistic(name: 'Notes', count: 32),
+                            SimpleCountStatistic(
+                                name: 'Transactions', count: 12),
+                            SimpleCountStatistic(name: 'Events', count: 140),
+                          ],
+                        ),
+                      ),
+                    ],
                   ),
-                ],
-              ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 10.0),
+                  child: FutureBuilder(
+                      future: context.read<AuthService>().username,
+                      builder: (context, snapshot) {
+                        if (snapshot.hasData)
+                          return Text(
+                            context
+                                    .read<AuthService>()
+                                    .currentUser
+                                    .displayName ??
+                                snapshot.data,
+                            style: TextStyle(
+                              fontSize: 18,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          );
+                        return CircularProgressIndicator();
+                      }),
+                ),
+              ],
             ),
           ),
         ],
