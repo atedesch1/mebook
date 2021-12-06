6f30f0a6bd4b1c99f6912256781134f46efe9661
Monk <kenjiyamane0@gmail.com>
2021-12-06 01:16:05 -0300

    add automatic adjust on invalid events

--- mobile/lib/widgets/schedule/calendar_utils.dart
+++ mobile/lib/widgets/schedule/calendar_utils.dart
@@ -1,6 +1,14 @@
 import 'package:googleapis/calendar/v3.dart' as googleCalendar;
 import 'package:flutter/material.dart';
 
+class TimeAggregate {
+  DateTime startDate;
+  TimeOfDay startTime;
+  DateTime endDate;
+  TimeOfDay endTime;
+  TimeAggregate(this.startDate, this.startTime, this.endDate, this.endTime);
+}
+
 googleCalendar.Event addTimeZone(googleCalendar.Event e) {
   e.start.dateTime = e.start.dateTime.toLocal();
   e.end.dateTime = e.end.dateTime.toLocal();
@@ -13,7 +21,7 @@ googleCalendar.Event removeTimeZone(googleCalendar.Event e) {
   return e;
 }
 
-DateTime constructDateTime(DateTime date, TimeOfDay timeOfDay) {
+DateTime joinDateTime(DateTime date, TimeOfDay timeOfDay) {
   return DateTime(
     date.year,
     date.month,
@@ -21,4 +29,36 @@ DateTime constructDateTime(DateTime date, TimeOfDay timeOfDay) {
     timeOfDay.hour,
     timeOfDay.minute,
   );
+}
+
+DateTime extractDate(DateTime d) {
+  return DateTime(d.year, d.month, d.day);
+}
+
+TimeOfDay extractTimeOfDay(DateTime d) {
+  return TimeOfDay.fromDateTime(d);
+}
+
+TimeAggregate adjustBeginToEnd(TimeAggregate timeAgg) {
+  DateTime begin = joinDateTime(timeAgg.startDate, timeAgg.startTime);
+  DateTime end = joinDateTime(timeAgg.endDate, timeAgg.endTime);
+  if (begin.isBefore(end)) {
+    return timeAgg;
+  }
+  DateTime adjustedBegin = end.subtract(Duration(hours: 1));
+  timeAgg.startDate = extractDate(adjustedBegin);
+  timeAgg.startTime = extractTimeOfDay(adjustedBegin);
+  return timeAgg;
+}
+
+TimeAggregate adjustEndToBegin(TimeAggregate timeAgg) {
+  DateTime begin = joinDateTime(timeAgg.startDate, timeAgg.startTime);
+  DateTime end = joinDateTime(timeAgg.endDate, timeAgg.endTime);
+  if (begin.isBefore(end)) {
+    return timeAgg;
+  }
+  DateTime adjustedEnd = begin.add(Duration(hours: 1));
+  timeAgg.endDate = extractDate(adjustedEnd);
+  timeAgg.endTime = extractTimeOfDay(adjustedEnd);
+  return timeAgg;
 }
