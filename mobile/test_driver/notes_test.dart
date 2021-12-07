2e0940e3564a2ccbfa7d316057c9b2229fda6084
Monk <kenjiyamane0@gmail.com>
2021-12-06 22:53:39 -0300

    refactor notes test group name

--- mobile/test_driver/notes_test.dart
+++ mobile/test_driver/notes_test.dart
@@ -2,7 +2,7 @@ import 'package:flutter_driver/flutter_driver.dart';
 import 'package:test/test.dart';
 
 void main() {
-  group('Mebook', () {
+  group('Notes', () {
     FlutterDriver driver;
 
     // connect to the Flutter driver before running any tests
